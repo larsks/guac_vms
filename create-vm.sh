dcdccd074add6c211003eb2278ed96ad20022aa5
Lars Kellogg-Stedman <lars@oddbit.com>
2022-03-04 11:22:28 -0500

    Set password from password file

--- create-vm.sh
+++ create-vm.sh
@@ -3,12 +3,22 @@
 username=$1
 slug=$(sed -E 's/[^a-z0-9-]+/-/g' <<<"$username")
 
-if ! [[ -f "keys/${username}.pub" ]]; then
+if ! [[ -f keys/${username}.pub ]]; then
 	echo "missing ssh key for $username" >&2
 	exit 1
 fi
 
-oc process -f template.yaml -p NAME="${slug}" -p SSHKEY="$(cat keys/"${username}.pub")" |
+if ! [[ -f keys/${username}.password ]]; then
+	echo "missing password for $username" >&2
+	exit 1
+fi
+
+oc process -f template.yaml \
+		-p NAME="${slug}" -p SSHKEY="none" -p CLOUD_USER_PASSWORD="none" |
 	oc delete -f- --wait=true
-oc process -f template.yaml -p NAME="${slug}" -p SSHKEY="$(cat keys/"${username}.pub")" |
+
+oc process -f template.yaml \
+		-p NAME="${slug}" \
+		-p SSHKEY="$(cat keys/"${username}.pub")" \
+		-p CLOUD_USER_PASSWORD="$(cat keys/"${username}.password")" |
 	oc apply -f-
